625d04dc643106520248e6ac356076f9d7c56c53
RSBAMM <dmchesney90@gmail.com>
2026-08-18 22:23:04 +0100

    Connect XP frontend to Supabase project

--- xp-config.js
+++ xp-config.js
@@ -1,5 +1,5 @@
 /* Public browser configuration only. Never place a Supabase service-role key here. */
 window.GANKBYTE_XP_CONFIG = {
-  supabaseUrl: "",
-  supabasePublishableKey: ""
+  supabaseUrl: "https://yedhmarvbohkjyagbdsm.supabase.co",
+  supabasePublishableKey: "sb_publishable_eMPo2VXGprIsaOm3ejXkRA_Fo8b_3oz"
 };
