1c30e2f6edad9dfcfd43a3684ce792ff1fe46af3
RSBAMM <dmchesney90@gmail.com>
2026-08-20 17:13:42 +0100

    Refresh account control stylesheet cache

--- footer-nav.js
+++ footer-nav.js
@@ -78,7 +78,7 @@
   }
   if (!document.querySelector('script[data-site-auth]')) {
     const script = document.createElement("script");
-    script.src = "site-auth.js?v=2";
+    script.src = "site-auth.js?v=3";
     script.dataset.siteAuth = "true";
     document.body.append(script);
   }
