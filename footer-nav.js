95feaae2edcba3a61f9832a4796ba4bfdbea6cdd
RSBAMM <dmchesney90@gmail.com>
2026-08-20 17:01:57 +0100

    Match global account button to site styling

--- footer-nav.js
+++ footer-nav.js
@@ -72,7 +72,7 @@
   if (!document.querySelector('link[data-site-auth-style]')) {
     const style = document.createElement("link");
     style.rel = "stylesheet";
-    style.href = "site-auth.css?v=1";
+    style.href = "site-auth.css?v=2";
     style.dataset.siteAuthStyle = "true";
     document.head.append(style);
   }
