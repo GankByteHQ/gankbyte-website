e5f20b95f2918b651946c32344702e8f793aae74
RSBAMM <dmchesney90@gmail.com>
2026-08-25 22:51:40 +0100

    Remove navigation dropdown indicators

--- footer-nav.js
+++ footer-nav.js
@@ -5,7 +5,7 @@
   if (!document.querySelector('link[data-site-shell-style]')) {
   const style = document.createElement("link");
     style.rel = "stylesheet";
-    style.href = "/site-shell.css?v=7";
+    style.href = "/site-shell.css?v=8";
     style.dataset.siteShellStyle = "true";
     document.head.append(style);
   }
